3b12643afa14cfb33802abf1f0f09c7eeb2826e4
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2026-01-14 13:15:37 +0300

    lib added

---
+++ C#/FirstProgram_Library/Even.cs
@@ -0,0 +1,18 @@
+﻿namespace FirstProgram_Library
+{
+    public class Even
+    {
+        public List<int> EvenList(int start, int end)
+        {
+            List<int> ints = new List<int>();
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                { 
+                    ints.Add(i); 
+                }
+            }
+            return ints;
+        }
+    }
+}
